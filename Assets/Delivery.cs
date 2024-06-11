9ad7cc59836add0d17b252cd56f5221644ca7d5b
Meengus22 <meengus22@gmail.com>
2024-06-10 22:19:17 -0500

    Finished through 2.17

---
+++ Assets/Delivery.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage = false;
+    
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Bonk");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package Picked Up");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package Delivered");
+            hasPackage = false;
+        }
+    }
+}
